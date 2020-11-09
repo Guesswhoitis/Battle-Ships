0ae1807fa950d439e82bf50f8eaa734f0b47309d
James Hulena <63756285+Guesswhoitis@users.noreply.github.com>
2020-11-10 06:21:47 +1300

    added main header

---
+++ main.hpp
@@ -0,0 +1,11 @@
+#ifndef EXISTS
+#define EXISTS
+#define SDL_MAIN_HANDLED
+#include <iostream>
+#include "SDL2-2.0.12/include/SDL_main.h"
+#include "SDL2-2.0.12/include/SDL.h"
+#include "SDL2-2.0.12/include/SDL_image.h"
+#include <stdio.h> 
+#include <winsock.h>
+#include <string.h> 
+#endif
